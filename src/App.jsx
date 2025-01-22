6b4439ab4b4c56d0fb8a21dceed651de06ba30c1
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-01-22 19:40:30 +0300

    main jsx base url fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TawkToWidget from "./services/TawkToWidget";
 import Home from './components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Contact from './components/Contact/Contact';
 import OurService from './components/OurService/OurService';
 import About from './components/About/About';
@@ -13,21 +13,19 @@ import PrivacyPolicy from './components/privacy policy/privacyPolicy';
 
 function App() {
   return (
-    <Router basename="/NileTaxServicees">
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<OurService />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/pp" element={<PrivacyPolicy />} />
-        </Routes>
-        <TawkToWidget />
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<OurService />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/pp" element={<PrivacyPolicy />} />
+      </Routes>
+      <TawkToWidget />
+      <Footer />
+    </>
   );
 }
 
