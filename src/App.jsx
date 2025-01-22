a76c6ef999caa0d613c823c195ba1310bab4a2e1
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-01-22 19:32:43 +0300

    router render fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contact from './components/Contact/Contact';
 import OurService from './components/OurService/OurService';
 import About from './components/About/About';
-import Miniheader from './components/Miniheader/Miniheader';
 import FAQ from './components/FAQ/FAQ';
 import PrivacyPolicy from './components/privacy policy/privacyPolicy';
 
 function App() {
   return (
-    <> 
+    <Router basename="/NileTaxServicees">
       <div>
-        <Router basename="/NileTaxServicees">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/services" element={<OurService />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/pp" element={<PrivacyPolicy />} />
-          </Routes>
-          <TawkToWidget />
-          <Footer />
-        </Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<OurService />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/pp" element={<PrivacyPolicy />} />
+        </Routes>
+        <TawkToWidget />
+        <Footer />
       </div>
-    </>
+    </Router>
   );
 }
 
